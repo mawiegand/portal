f3ac94772740d751483733f222fb027bcabc4a2d
Sascha Lange <sascha77@googlemail.com>
2012-05-07 16:56:33 +0200

    initial commit of awe portal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portal::Application.config.secret_token = '17fbb05412eedce723d235c12cf195679e6f1cce90ca2a9dfc2b84ffc3653b6fa791f6f5dc800771ab6ddf47858b102c0754f0296619a4f17086f228357bdf98'
