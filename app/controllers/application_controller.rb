ee6d754ba965f03d7a8dda7057f7bb66448fffee
Sascha Lange <sascha77@googlemail.com>
2012-05-07 18:24:03 +0200

    added controller and locale for landing page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,37 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_locale  # get the locale from the user parameters
+  around_filter :time_action
+
+  
+  # This method adds the locale to all rails-generated path, e.g. root_path.
+  # Based on I18n documentation in rails guides:
+  # http://guides.rubyonrails.org/i18n.html  
+  def default_url_options(options={})
+    { :locale => I18n.locale }
+  end
+  
+  
+  protected
+    
+    def time_action
+      started = Time.now
+      yield
+      elapsed = Time.now - started
+      logger.debug("Executing #{controller_name}::#{action_name} took #{elapsed*1000}ms in real-time.")
+    end
+  
+    # Set the locale according to the user specified locale or to the default
+    # locale, if not specified or specified is not available.
+    def set_locale
+      I18n.locale = get_locale_from_params || I18n.default_locale
+    end
+    
+    # Checks whether the user specified locale is available.
+    def get_locale_from_params 
+      return nil unless params[:locale]
+      I18n.available_locales.include?(params[:locale].to_sym) ? params[:locale] : nil
+    end
+  
 end
