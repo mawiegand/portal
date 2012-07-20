14893b0243abff7cbc8af55d78bd71fab96f328b
Sascha Lange <sascha77@googlemail.com>
2012-07-20 11:40:15 +0200

    invitation parameter to javascript -> window.invitation

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,6 +2,7 @@ class HomePageController < ApplicationController
   
   def show
     @title = I18n.t('home_page.welcome')
+    @invitation = params[:invitation]      if params[:invitation]
   end
 
 end
