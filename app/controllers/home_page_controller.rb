dba7324d7e658d669d73b5b983ee6b5c1a82aaa7
Your Name <p@trick-fox.de>
2012-07-28 11:08:45 +0200

    localization

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -4,22 +4,21 @@ class HomePageController < ApplicationController
   def show
     @title = I18n.t('home_page.welcome')
     
-    @invitation     = params[:invitation]      if params[:invitation]
-    
+    @invitation    = params[:invitation]      if params[:invitation]
     id             = params[:id]              
     password_token = params[:password_token]
 
     if !id.blank? && !password_token.blank?
-      response = HTTParty.get(PORTAL_CONFIG['identity_provider_base_url'] + '/send_password', 
+      response = HTTParty.get(PORTAL_CONFIG['identity_provider_base_url'] + '/' + I18n.locale.to_s + '/send_password', 
                    :query   => {:id              => id,
                                 :password_token  => password_token,
                                 :client_id       => PORTAL_CONFIG['client_id'],
                                 :client_password => PORTAL_CONFIG['client_password'] },
                    :headers => { 'Accept' => 'application/json'})
       if response.code == 200
-        @message = 'Wir haben dir ein neues Passwort an deine Mail-Adresse geschickt. Du kannst dich damit wieder einloggen.' 
+        @message = I18n.t('home_page.password_sent')
       else
-        @message = 'Das Passwort Token wurde nicht gefunden'
+        @message = I18n.t('home_page.password_not_sent')
       end             
     end
   end
