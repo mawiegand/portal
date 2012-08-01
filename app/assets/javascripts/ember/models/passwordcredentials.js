ffa825f67108147dc53e5f81178db80c1f0d4801
Your Name <p@trick-fox.de>
2012-08-01 19:20:15 +0200

    added temporary observers for debugging

--- app/assets/javascripts/ember/models/passwordcredentials.js
+++ app/assets/javascripts/ember/models/passwordcredentials.js
@@ -1,7 +1,23 @@
 
 Portal.PasswordCredentials = Ember.Object.extend({
   email: null,
+  password1: null,
+  password2: null,
   password: null,
+  
+  passwordObserver: function(){
+    console.log('---> password', this.get('password'), this);
+  }.observes('password'),
+  
+  password1Observer: function(){
+    this.set('password', this.get('password1'));
+    console.log('---> password1', this.get('password'));
+  }.observes('password1'),
+  
+  password2Observer: function(){
+    this.set('password', this.get('password2'));
+    console.log('---> password2', this.get('password'));
+  }.observes('password2'),
   
   validate: function() {
     if (this.get('email') === undefined || this.get('email') === '' ||
