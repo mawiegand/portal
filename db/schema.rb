3f44aee127d6b1be3a5ef112277e97b0d9da2b3d
Marcel Wiegand <marcel@bluepanel.org>
2012-09-20 17:35:54 +0200

    Revert "display only announcements with user locale"
    This reverts commit 1e58e4b1665bbb9b779b1df08e1ccd3893775dad.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120920005325) do
+ActiveRecord::Schema.define(:version => 20120918233205) do
 
   create_table "announcements", :force => true do |t|
     t.string   "title"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(:version => 20120920005325) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "locale"
   end
 
 end
