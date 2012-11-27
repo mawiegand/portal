7d74023454a481fe0e02618c0ecd7551c5830464
salange <sascha@5dlab.com>
2012-11-28 00:13:03 +0100

    routes in locale

--- config/routes.rb
+++ config/routes.rb
@@ -10,19 +10,20 @@ Portal::Application.routes.draw do
 
     resources :home_page,     :only => [ :show ]
 
-    match :index, :to => 'home_page#show'
-    match '/invitations/:invitation', :to => 'home_page#show'
-    match '/new_password/:id/:password_token', :to => 'home_page#show'
+    match :index,                                      :to => 'home_page#show'
+    match '/invitations/:invitation',                  :to => 'home_page#show'
+    match '/new_password/:id/:password_token',         :to => 'home_page#show'
+    
+    match '/return/:retention',                        :to => 'home_page#show'
+    match '/invitations/:invitation',                  :to => 'home_page#show'
+    match '/player_invitation/:player_invitation',     :to => 'home_page#show'
+    match '/alliance_invitation/:alliance_invitation', :to => 'home_page#show'
+    match '/new_password/:id/:password_token',         :to => 'home_page#show'    
+    
   end
 
   root :to => 'home_page#show'
   match :index, :to => 'home_page#show'
   match '/:locale' => 'home_page#show'
-  
-  match '/return/:retention', :to => 'home_page#show'
-  match '/invitations/:invitation', :to => 'home_page#show'
-  match '/player_invitation/:player_invitation', :to => 'home_page#show'
-  match '/alliance_invitation/:alliance_invitation', :to => 'home_page#show'
-  match '/new_password/:id/:password_token', :to => 'home_page#show'
   
 end
