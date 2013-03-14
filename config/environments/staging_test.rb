9a2b6b75c1567ab41847acc389c30f5fb8711fa3
Patrick <p@trick-fox.de>
2013-03-14 21:42:59 +0100

    bugfix

--- config/environments/staging_test.rb
+++ config/environments/staging_test.rb
@@ -58,4 +58,6 @@ Portal::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.ember.variant = :staging_test
 end
