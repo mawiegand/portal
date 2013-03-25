12e366bfea60bba77f62ce4833f16b97c5201961
Patrick <p@trick-fox.de>
2013-03-25 16:10:12 +0100

    bug found and fixed

--- config/deploy/round2.rb
+++ config/deploy/round2.rb
@@ -2,6 +2,8 @@ role :web, "backup-round2.wack-a-doo.de"                          # Your HTTP se
 role :app, "backup-round2.wack-a-doo.de"                          # This may be the same as your `Web` server
 role :db,  "backup-round2.wack-a-doo.de", :primary => true        # This is where Rails migrations will run
 
+set :branch,    'backup-round2'
+
 set :rails_env, 'round2'
 set :port, 5775                                            # until 5775 is working!
 
