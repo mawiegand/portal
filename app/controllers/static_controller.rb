8f1362c88c2b5715400184fc5be616a8bd614469
salange <sascha@5dlab.com>
2013-06-04 19:48:34 +0200

    debug output

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -11,7 +11,8 @@ class StaticController < ApplicationController
     @game_id      = 1
     @round_number = params[:round_number] || 0
     
-    hostname = request.host
+    @hostname = request.host
+    
     
     response = HTTParty.get("https://#{hostname}/identity_provider/resource/results.json?game_id=#{ @game_id }&round_number=#{ @round_number }")
     @content = response || [];
