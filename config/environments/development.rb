cd53ad719d567f8e7010385d2a71fb40182f600e
Sascha Lange <sascha@5dlab.com>
2014-09-16 22:32:21 +0200

    working on sampl in portal

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,7 @@ Portal::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  #config.assets.compile = false
 
   # Expands the lines which load the assets
   config.assets.debug = true
