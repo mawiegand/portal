6b24385511e950695991e76ec2abf5ada38bfe74
Patrick Fox <patrick.fox@ext.imperia-ag.de>
2014-11-23 19:43:09 +0100

    own portal for round7

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,7 @@
-role :web, "portal.wack-a-doo.de"                          # Your HTTP server, Apache/etc
-role :app, "portal.wack-a-doo.de"                          # This may be the same as your `Web` server
-role :db,  "portal.wack-a-doo.de", :primary => true        # This is where Rails migrations will run
+role :web, "gs07.wack-a-doo.de"                          # Your HTTP server, Apache/etc
+role :app, "gs07.wack-a-doo.de"                          # This may be the same as your `Web` server
+role :db,  "gs07.wack-a-doo.de", :primary => true        # This is where Rails migrations will run
 
 set :rails_env, 'production'
+set :branch,    'round7'
 set :port, 5775                                            # until 5775 is working!
