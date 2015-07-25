ae08fb1f9c62bfa83feaa1ed1849767673c5a547
Patrick Fox <patrick.fox@ext.imperia-ag.de>
2015-07-25 18:47:30 +0200

    own temporary local portal for round 9

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,8 @@
-role :web, "portal.wack-a-doo.de"                          # Your HTTP server, Apache/etc
-role :app, "portal.wack-a-doo.de"                          # This may be the same as your `Web` server
-role :db,  "portal.wack-a-doo.de", :primary => true        # This is where Rails migrations will run
+role :web, "gs09.wack-a-doo.de"                          # Your HTTP server, Apache/etc
+role :app, "gs09.wack-a-doo.de"                          # This may be the same as your `Web` server
+role :db,  "gs09.wack-a-doo.de", :primary => true        # This is where Rails migrations will run
 
 set :rails_env, 'production'
 set :port, 5775                                            # until 5775 is working!
+
+set :branch,    'round9'
